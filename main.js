417f79b4c28f3dc8b4d6a3c668e0a6f344f6719d
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-11-23 23:34:13 -0500

    made nasa work

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 
 
-// const text = document.querySelector('input').value
-const big = function(){
-      return document.querySelector('input').value=biggify(document.querySelector('input').value)
-}
-document.querySelector('.biggify').addEventListener('click',big)
 
+const big = function(){
+    return document.querySelector('input').value=biggify(document.querySelector('input').value)
+}
+
+const nasa = function(){
+    return document.querySelector('input').value = nasafy(document.querySelector('input').value)
+}
+
+const bonkers = function(){
+    return document.querySelector('input').value = crazify(document.querySelector('input').value)
+}
+
+document.querySelector('.biggify').addEventListener('click',big)
+document.querySelector('.nasafy').addEventListener('click', nasa)
+document.querySelector('.crazify').addEventListener('click', bonkers)
