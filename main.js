144e717729a5bce721b8e9537d22ec27b2aa014f
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-11-23 21:23:18 -0500

    made biggify work

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+// const text = document.querySelector('input').value
+const big = function(){
+      return document.querySelector('input').value=biggify(document.querySelector('input').value)
+}
+document.querySelector('.biggify').addEventListener('click',big)
+
