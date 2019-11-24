5325a6b9561a16bff8252e58091fe513ca831bec
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-11-24 00:13:19 -0500

    made titile case work

--- main.js
+++ main.js
@@ -13,6 +13,18 @@ const bonkers = function(){
     return document.querySelector('input').value = crazify(document.querySelector('input').value)
 }
 
+const backWards = function(){
+    return document.querySelector('input').value = reversify(document.querySelector('input').value)
+}
+
+const movie = function(){
+    return document.querySelector('input').value = titleify(document.querySelector('input').value)
+}
+
+
+
 document.querySelector('.biggify').addEventListener('click',big)
 document.querySelector('.nasafy').addEventListener('click', nasa)
 document.querySelector('.crazify').addEventListener('click', bonkers)
+document.querySelector('.reversify').addEventListener('click', backWards)
+document.querySelector('.titleify').addEventListener('click', movie)
